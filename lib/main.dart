d65809761ffb6ee5c3500160a93dec378203c9e6
MatiasSosa <fuxesosa@gmail.com>
2023-10-24 13:33:47 -0300

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:front2tp/page/categoria_page.dart';
+import 'package:front2tp/page/editar_categoria.dart';
 import 'package:front2tp/page/home_page.dart';
 
 void main() {
@@ -8,31 +10,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return MaterialApp(
       title: 'Front 2P',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+      initialRoute: '/',
+      // Aqui se agregan las rutas de cada seccion de la app
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/categoria': (context) => const CategoriaPage(),
+        '/editarCategoria': (context) => EditarCategoria(),
+      },
     );
   }
 }
